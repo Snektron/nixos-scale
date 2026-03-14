864cdb8a46bb5ba8386461385fb2f5abb84ba8fd
Robin Voetter <robin@voetter.nl>
2026-03-14 23:41:33 +0100

    update to scale 1.6.1

--- flake.nix
+++ flake.nix
@@ -44,10 +44,10 @@
         makeWrapper,
       }: stdenv.mkDerivation (finalAttrs: rec {
         pname = "scale";
-        version = "1.5.1";
+        version = "1.6.1";
         src = fetchurl {
           url = "https://pkgs.scale-lang.com/tar/scale-${version}-amd64.tar.xz";
-          hash = "sha256-dzG6H0VQ+ZZO+lH4vPbB/6nG9bdVyWDyrYpXbQI6/Kg=";
+          hash = "sha256-3k8NfT3fjmVlO9ASAckuhEGX+NU2bol10cfP6K6n5e4=";
         };
         dontConfigure = true;
         dontBuild = true;
